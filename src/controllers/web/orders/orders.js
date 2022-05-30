c95f3ff937e66aec446b0358fde01c278992d83e
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-05-30 19:53:39 +0500

    add getOne ans verification

--- src/controllers/web/orders/orders.js
+++ src/controllers/web/orders/orders.js
@@ -1,6 +1,6 @@
 const { Op } = require("sequelize")
 const sequelize = require("../../../db/db")
-const { users, orders } = sequelize.models
+const { users, orders, order_items } = sequelize.models
 
 class OrdersController{
     static async GetAll(req, res, next) {
@@ -27,6 +27,82 @@ class OrdersController{
                     orders: allOrders.rows,
                     count: allOrders.count
                 }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async GetOne(req, res, next) {
+        try {
+            const { query, params } = req
+
+            const order = await orders.findOne({
+                where: {
+                    id: params.id
+                },
+                include: [
+                    {
+                        model: users
+                    },
+                    {
+                        model: order_items
+                    }
+                ]
+            })
+
+            res.status(200).json({
+                ok: true,
+                data: {
+                    order
+                }
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async SetCost(req, res, next) {
+        try {
+            const { query, params, body } = req
+
+            const order = await orders.update({
+                price: body.cost,
+                text: body.text,
+                status: 2
+            },{
+                where: {
+                    id: params.id
+                }
+            })
+
+            res.status(200).json({
+                ok: true,
+                message: "Order updated"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async Verification(req, res, next) {
+        try {
+            const { query, params, body } = req
+
+            let status = query.verified == true ? 4 : 3
+
+            const order = await orders.update({
+                is_paid: query.verified,
+                status: status
+            },{
+                where: {
+                    id: params.id
+                }
+            })
+
+            res.status(200).json({
+                ok: true,
+                message: "Order updated"
             })
         } catch (error) {
             next(error)
