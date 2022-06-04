7d1d3b628dc01d1004df1a985ef70150d4f0c066
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-04 18:31:27 +0500

    cors update

--- src/server.js
+++ src/server.js
@@ -10,13 +10,13 @@ require("dotenv").config()
 
 const app = express();
 
+app.use(cors({ origin: "*" }))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use("uploads", express.static(path.join(__dirname, "src", "uploads")))
 
 app.use(errorMiddleware)
-app.use(cors())
 
 app.use("/api", router)
 app.use(errorHandler)
