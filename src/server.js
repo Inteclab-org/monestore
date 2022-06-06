db54a7773f212be44f6a53e6da244f6f7aadd08f
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-06 16:31:11 +0500

    test

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use("/resources", express.static(path.join(__dirname, "uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 app.use(errorMiddleware)
 
