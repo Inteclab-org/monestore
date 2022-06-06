a7fd64e4f5c5c78bae552f2899452b8aad87f88c
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-06 16:14:19 +0500

    fix

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use("uploads", express.static(path.join(__dirname, "src", "uploads")))
+app.use("/uploads", express.static(path.join(__dirname, "src", "uploads")))
 
 app.use(errorMiddleware)
 
