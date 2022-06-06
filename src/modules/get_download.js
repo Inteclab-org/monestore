70abdc0db43545fde8c1e40c8fac690c122461e9
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-06 16:44:05 +0500

    change downloader

--- src/modules/get_download.js
+++ src/modules/get_download.js
@@ -1,4 +1,5 @@
 const request = require("request")
+const http = require("http")
 const fs = require("fs")
 const path = require("path")
 const configs = require('../config')
@@ -7,11 +8,21 @@ module.exports.fileDownloader = async function(file){
     try {
         let url = `https://api.telegram.org/file/bot${configs.TG_TOKEN}/photos/${file.file_path}`
 
-        request.head(url, (err, res, body) => {
-            request(url).pipe(fs.createWriteStream(path.join(__dirname, "..", "uploads", "files", file.file_id + ".jpg"))).on('close', () => {
-                console.log("file saved");
+        // request.head(url, (err, res, body) => {
+        //     request(url).pipe(fs.createWriteStream(path.join(__dirname, "..", "uploads", "files", file.file_id + ".jpg"))).on('close', () => {
+        //         console.log("file saved");
+        //     });
+        //   });
+
+        const f = fs.createWriteStream(path.join(__dirname, "..", "uploads", "files", file.file_id + ".jpg"))
+        const request = http.get(url, function(response){
+            response.pipe(f)
+
+            f.on("finish", () => {
+                f.close();
+                console.log("Download Completed");
             });
-          });
+        })
     } catch (error) {
         console.log(error);
     }
