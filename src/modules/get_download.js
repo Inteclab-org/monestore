14564623ca9abddde7e0476791f0c0d9d79be3f3
Farrukh Olimov <farruhbekolimov3904@gmail.com>
2022-06-06 16:29:12 +0500

    test

--- src/modules/get_download.js
+++ src/modules/get_download.js
@@ -1,10 +1,11 @@
 const request = require("request")
 const fs = require("fs")
 const path = require("path")
+const configs = require('../config')
 
 module.exports.fileDownloader = async function(file){
     try {
-        let url = `https://api.telegram.org/file/bot5384545061:AAG76jxC0S9gOwcJJ6tmjDJ4vlXDtkzKcOg/photos/${file.file_path}`
+        let url = `https://api.telegram.org/file/bot${configs.TG_TOKEN}/photos/${file.file_path}`
 
         request.head(url, (err, res, body) => {
             request(url).pipe(fs.createWriteStream(path.join(__dirname, "..", "uploads", "files", file.file_id + ".jpg"))).on('close', () => {
